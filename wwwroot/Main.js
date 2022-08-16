a69c9a9a7fba333a32b15eaf0cc4737dd9b7d7a3
benjimola <benjamin.ayt@hotmail.co.uk>
2022-08-16 11:16:12 +0100

    Add project files.

---
+++ wwwroot/Main.js
@@ -0,0 +1,30 @@
+﻿
+function mainSlider() {
+
+
+
+    $('.owl-main').owlCarousel({
+        loop: true,
+        margin: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplay: true,
+        autoplayTimeout: 7000,
+        autoplayHoverPause: false,
+        responsive: {
+            320: {
+                items: 1
+            }
+            ,
+            1920: {
+                items: 1
+            }
+        }
+    });
+}
+
+function mainModal(){
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+      })
+}
